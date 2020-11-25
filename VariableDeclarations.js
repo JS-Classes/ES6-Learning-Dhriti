857cbdb8447dd2a6c00706bbd5474ad8f6af5911
Dhriti Shakya <i260014@d2hs.com>
2020-11-25 13:03:26 +0545

    Variable Declaration in JS

---
+++ VariableDeclarations.js
@@ -0,0 +1,22 @@
+//Datatypes
+//Primary
+var a='Dhriti'; //single quote or double quote
+var b=true;
+var c=5467;
+var d=2n ** 200n;
+var e = undefined;
+var f = null;
+
+//Structural
+var g =[2,3,4,5];
+var h = {name:'Dhriti',isFemale:true};
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
