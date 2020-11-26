ecbdd6316bc19e68355a5e5a5e78e8b0f8e18111
Dhriti Shakya <i260014@d2hs.com>
2020-11-26 09:20:13 +0545

    All about JS Datatypes

--- VariableDeclarations.js
+++ VariableDeclarations.js
@@ -1,22 +1,38 @@
 //Datatypes
 //Primary
-var a='Dhriti'; //single quote or double quote
-var b=true;
-var c=5467;
-var d=2n ** 200n;
+var a ='Dhriti'; //single quote or double quote
+var b =true;
+var c =5467;
+var c1 = +Infinity;
+var c2 = -Infinity;
+var c3 = NaN; //not a number
+var x ='dfsfsg'/34;
+console.log(x);
+var d =2n ** 200n;
 var e = undefined;
 var f = null;
 
 //Structural
 var g =[2,3,4,5];
 var h = {name:'Dhriti',isFemale:true};
+var i = ['a','b','c'];
 
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
-console.log(g);
-console.log(h);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+console.log(typeof i);
