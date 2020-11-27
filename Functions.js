15ed02f8012f900d3d59fb8dd6b750313c91a1fd
Dhriti Shakya <i260014@d2hs.com>
2020-11-27 09:58:32 +0545

    Learn more about functions and parameters

--- Functions.js
+++ Functions.js
@@ -19,3 +19,83 @@ var func1=function (number)
 }
 let sa=func1(561);
 console.log(sa);
+
+//Arrow function
+var fun2=number=> number*number;
+
+(a,b) => {
+   let sum = a + b;
+   return sum;
+}
+
+//Closure
+function outside(x)
+{
+function inside(y)
+{
+   return x + y;
+}
+return inside;
+}
+
+let inside_func = outside(10); // Give me a function that adds 10 to whatever comes to it
+let result = inside_func(40);
+console.log(result);
+
+function addSquares(a,b){
+
+   function square(x){
+      return x * x;
+   }
+
+   let result = square(a) + square(b);
+   return result;
+}
+
+let r = addSquares(3453,234);
+
+//Example 3
+let createPet = function (name)
+{
+   return{
+      setName: function(newName){
+         name=newName;
+      },
+      getName: function(){
+         return name;
+      }
+   }
+}
+
+let dog = createPet('Rocky');
+dog.setName('');
+
+//Using Arrow function
+let createPet = function (name)
+{
+   return{
+      setName: newName => name = newName,
+      getName: () => name
+         
+      }
+ }
+
+ //Parameters
+ function formatName(name,address,salutation="Ms."){
+    let completeName = salutation + "+" + name + "+" + address;
+   return completeName;
+}
+let n = formatName("Bishnu","abc","Mr");
+let n1 = formatName("Bishnu","abc");
+console.log(n1);
+
+
+//Rest parameters
+function add (a,b,...moreArgs){
+   console.log(a);
+   console.log(a);
+   console.log(a);
+}
+
+add (23,45);
+add (23,34,45,56,67);
